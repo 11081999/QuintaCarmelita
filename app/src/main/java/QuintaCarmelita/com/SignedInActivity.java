1bd62cd09b45842e473fc21d7e054a9c4a790b18
Roberto Rivera Terán <33244698+11081999@users.noreply.github.com>
2020-10-23 01:48:00 -0500

    Firebase Login With SDK (Test)

---
+++ app/src/main/java/QuintaCarmelita/com/SignedInActivity.java
@@ -0,0 +1,44 @@
+/*
+package QuintaCarmelita.com;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignedInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signed_in);
+    }
+
+    public void onLogout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+    public void getUserInformation() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to authenticate with your backend server, if you  have one. Use FirebaseUser.getToken() instead
+            String uid = user.getUid();
+        }
+
+    }
+}
+ */
