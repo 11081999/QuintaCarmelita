34a0afbf4c9cd960225122687025ca9276298dbf
Roberto Rivera Terán <33244698+11081999@users.noreply.github.com>
2020-10-31 17:19:04 -0600

    New UI for Login

---
+++ app/src/main/java/QuintaCarmelita/com/DatabaseActivity.java
@@ -0,0 +1,25 @@
+/*
+package QuintaCarmelita.com;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class DatabaseActivity {
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFireBaseInstance;
+    private String userId;
+
+    onCreate
+}
+*/
+
