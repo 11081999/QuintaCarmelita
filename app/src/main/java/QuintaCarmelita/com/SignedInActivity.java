13dd3ed696a2d056d03ba8c1a842c08896a37b65
Alberto <albertofdomriv@gmail.com>
2020-11-04 16:33:51 -0600

    modificaciones1

--- app/src/main/java/QuintaCarmelita/com/SignedInActivity.java
+++ app/src/main/java/QuintaCarmelita/com/SignedInActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,10 +13,20 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class SignedInActivity extends AppCompatActivity {
 
+    Button btn_bs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signed_in);
+        btn_bs =findViewById(R.id.button);
+        btn_bs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SignedInActivity.this, LogActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void onLogout(View view) {
