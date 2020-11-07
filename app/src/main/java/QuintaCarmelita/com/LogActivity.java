ab790dbaf5f10763e5bee082f5f3b0bcfae9dd54
Roberto Rivera Terán <33244698+11081999@users.noreply.github.com>
2020-11-07 15:42:57 -0600

    Adatation & creation of kid object

--- app/src/main/java/QuintaCarmelita/com/LogActivity.java
+++ app/src/main/java/QuintaCarmelita/com/LogActivity.java
@@ -2,12 +2,11 @@ package QuintaCarmelita.com;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+//import com.google.android.material.floatingactionbutton.FloatingActionButton;
+//import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 
 public class LogActivity extends AppCompatActivity {
